87bdf81d53283be04b0f2a33bb626dce55e759a6
ClemLanoe <clementlanoe@outlook.com>
2023-04-08 18:02:45 +0100

    Added allowed host

--- my_seedrs/myseedrs/settings.py
+++ my_seedrs/myseedrs/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
 
 # Application definition
