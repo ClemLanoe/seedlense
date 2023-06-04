49797dc6f6d7c58b7f53dbf79a0c14d581ed0a56
ClemLanoe <clementlanoe@outlook.com>
2023-06-04 17:32:57 +0100

    Added js static file for testing

--- src/myseedrs/settings.py
+++ src/myseedrs/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = "/var/www/myseedrs/static"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
