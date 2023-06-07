d1bf6a7eb7a8ecc5c531f188155798e4fabf290a
ClemLanoe <clementlanoe@outlook.com>
2023-06-07 19:22:02 +0100

    Changing to dev mode

--- src/myseedrs/settings.py
+++ src/myseedrs/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOST'], "127.0.0.1", ".clemlanoe.com"]
 
